c932737bafa5d8dcd629fcf7a453dc7073b1208f
DiabolikElements <jonpierre.pretorius90@gmail.com>
2022-11-24 10:34:02 +0200

    new addeds

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,20 +5,20 @@ import Login from '../components/Login';
 import Register from '../components/Register';
 
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <div className="container">
-                <Switch>
-                    <Route path='/' component={Login} exact={true}/>
-                    <Route path='/register' component={Register}/>
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Login} exact={true} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
+  auth: state.auth
 });
 
 export default connect(mapStateToProps)(AppRouter);
